036bbe3fd04923e8ae00771c359d50b635086564
Andrew Arrow <andrew@bird.co>
2019-06-24 03:10:52 -0700

    better

--- uds/service_transfer.go
+++ uds/service_transfer.go
@@ -10,4 +10,9 @@ func service_transfer_make_request(seqnum byte, data []byte) *Request {
 func service_transfer_handle_response(r *Response) {
 
 	r.service_data["sequence_number_echo"] = r.data[0]
+	r.service_data["parameter_records"] = []byte{}
+	if len(r.data) > 1 {
+		r.service_data["parameter_records"] = r.data[1:]
+	}
+
 }
--- uds/uds_test.go
+++ uds/uds_test.go
@@ -9,7 +9,7 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestClient(t *testing.T) {
+func TestTransferData(t *testing.T) {
 	conn := NewQueueConnection("test", 4095)
 	client := NewClient(conn, 0.2)
 
@@ -20,10 +20,9 @@ func TestClient(t *testing.T) {
 		conn.fromuserm.Unlock()
 	}()
 
-	go func() {
-		response := client.transfer_data(0x22, []byte{0x12, 0x34, 0x56})
-		eq(t, response.service_data["sequence_number_echo"], 0x22, "TestClient")
-	}()
+	response := client.transfer_data(0x22, []byte{0x12, 0x34, 0x56})
+	eq(t, response.service_data["sequence_number_echo"], 0x22, "TestTransferData")
+	eq(t, response.service_data["parameter_records"], []byte{0x89, 0xab, 0xcd, 0xef}, "TestTransferData")
 
 	time.Sleep(20 * time.Millisecond)
 }
