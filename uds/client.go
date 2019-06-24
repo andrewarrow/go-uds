7768b9ce9366f3183795dfba413c10e3e77ea8f3
Andrew Arrow <andrew@bird.co>
2019-06-24 10:05:42 -0700

    Revert "reflection"
    This reverts commit 228107e1744fa0d14f3e00a5a001ed2d6126cc40.

--- uds/client.go
+++ uds/client.go
@@ -1,53 +1,23 @@
 package uds
 
-import "fmt"
-import "strings"
+//import "fmt"
+import "github.com/andrewarrow/go-isotp/isotp"
 
 type Client struct {
-	conn                       interface{}
+	conn                       isotp.AnyConn
 	timeout                    float32
 	suppress_positive_response bool
 }
 
-func NewClient(connection interface{}, timeout float32) *Client {
+func NewClient(connection isotp.AnyConn, timeout float32) *Client {
 	c := Client{}
 	c.conn = connection
 	c.suppress_positive_response = true
 	return &c
 }
 
-func Empty_rxqueue(conn interface{}) {
-	name := fmt.Sprintf("%v", conn)
-	tokens := strings.Split(name[2:], " ")
-	fmt.Println(tokens[0])
-	if tokens[0] == "queue" {
-		qc := conn.(*QueueConnection)
-		qc.Empty_rxqueue()
-	}
-}
-func Wait_frame(conn interface{}) []byte {
-	name := fmt.Sprintf("%v", conn)
-	tokens := strings.Split(name[2:], " ")
-
-	if tokens[0] == "queue" {
-		qc := conn.(*QueueConnection)
-		return qc.Wait_frame()
-	}
-
-	b := []byte{}
-	return b
-}
-func Send(conn interface{}, data []byte) {
-	name := fmt.Sprintf("%v", conn)
-	tokens := strings.Split(name[2:], " ")
-	if tokens[0] == "queue" {
-		qc := conn.(*QueueConnection)
-		qc.Send(data)
-	}
-}
-
 func (c *Client) send_request(request *Request) *Response {
-	Empty_rxqueue(c.conn)
+	c.conn.Empty_rxqueue()
 	payload := []byte{}
 	//override_suppress_positive_response := false
 	if c.suppress_positive_response && request.use_subfunction {
@@ -56,8 +26,8 @@ func (c *Client) send_request(request *Request) *Response {
 	} else {
 		payload = request.get_payload(false)
 	}
-	Send(c.conn, payload)
-	data := Wait_frame(c.conn)
+	c.conn.Send(payload)
+	data := c.conn.Wait_frame()
 	response := response_from_payload(data)
 	return response
 }
--- uds/go.mod
+++ uds/go.mod
@@ -1,3 +1,5 @@
 module uds
 
 go 1.13
+
+require github.com/andrewarrow/go-isotp v0.0.12
