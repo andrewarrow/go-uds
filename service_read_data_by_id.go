966e577ec60c504ddda081005fb9b40929e6ce47
Andrew Arrow <andrew@bird.co>
2019-06-25 11:37:44 -0700

    did fix

--- service_read_data_by_id.go
+++ service_read_data_by_id.go
@@ -2,10 +2,17 @@ package uds
 
 import "fmt"
 
-func service_read_data_by_id_make_request(seqnum byte, data []byte) *Request {
+func byteArrayWithZeros(data []byte) []byte {
+	buff := []byte{}
+	for _, b := range data {
+		buff = append(buff, 0, b)
+	}
+	return buff
+}
+
+func service_read_data_by_id_make_request(data []byte) *Request {
 	r := NewRequest(0x22, "read_data_by_id")
-	r.data = []byte{seqnum}
-	r.data = append(r.data, data...)
+	r.data = byteArrayWithZeros(data)
 	r.use_subfunction = false
 	return r
 }
